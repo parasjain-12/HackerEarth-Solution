705fb4fec0c5b6843f1b7f0cd39a6789f754ccbf
Paras Jain <jainparas249@gmail.com>
2018-09-03 15:21:32 +0530

    Create  Memorise Me!.py

---
+++ Memorise Me!.py
@@ -0,0 +1,12 @@
+# Write your code here
+from collections import Counter
+n= int(input())
+a = list(map(int,input().split()))
+m = int(input())
+p = Counter(a)
+for i in range(m):
+    x = int(input())
+    if p[x]:
+        print(p[x])
+    else:
+        print('NOT PRESENT')
