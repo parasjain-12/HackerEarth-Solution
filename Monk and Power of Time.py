b4526ca67cc5f73cab72a37902fb0dca932aae8d
Paras Jain <jainparas249@gmail.com>
2019-08-14 15:35:25 +0530

    Update Monk and Power of Time.py

--- Monk and Power of Time.py
+++ Monk and Power of Time.py
@@ -1,19 +1,21 @@
 n = int(input())
+call = list(map(int,input().split()))
+ideal = list(map(int,input().split()))
 
-cal = list(map(int,input().split()))
-ide = list(map(int,input().split()))
-j=0
-t=0
-while(j!=n):
-    #print('xxx')
-    if (cal[0] == ide[0]):
-        t+=1
-        x=cal.pop(0)
-        y = ide.pop(0)
-        j+=1
+def check(a,b):
+    if a[0] == b[0]:
+        return True
+    return False
+
+c=0
+while len(call)>0:
+    if check(call,ideal)==True:
+        call = call[1:]
+        ideal = ideal[1:]
+        c+=1
     else:
-        x = cal.pop(0)
-        cal.append(x)
-        #print(cal)
-        t+=1
-print(t)
+        call = call[1:n]+call[:1]
+        #print(call)
+        c+=1
+
+print(c)
