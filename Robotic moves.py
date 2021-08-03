be4a76f865605401216d9094990768ad444d2549
Paras Jain <jainparas249@gmail.com>
2021-08-03 22:20:21 +0530

    Create Robotic moves.py

---
+++ Robotic moves.py
@@ -0,0 +1,7 @@
+# We only need to calculate for x axis since y axis part will be zero.
+#For any more query feel free to mail me.
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(n*(n+1))
